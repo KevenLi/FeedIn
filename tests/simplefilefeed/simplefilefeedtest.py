58211b9beae89db9cc165c0f64edbe1d7bb26f01
Keven Li <pbleester@gmail.com>
2015-04-28 11:42:06 +0800

    test cases configuration update

--- tests/simplefilefeed/simplefilefeedtest.py
+++ tests/simplefilefeed/simplefilefeedtest.py
@@ -7,6 +7,7 @@ Created on 2014��11��12��
 import unittest
 import feedin.engine
 import json
+import os
 
 
 class SimpleFileFeedTest(unittest.TestCase):
@@ -19,11 +20,16 @@ class SimpleFileFeedTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+    
+    def get_conf_file(self, file):
+        dir = os.path.dirname(__file__)
+        return os.path.join(dir, file)
 
 
     def test_start(self):
         setting_file = 'sample.feed.json'
-        with open(setting_file, 'r') as f:
+        file_path = self.get_conf_file(setting_file)
+        with open(file_path, 'r') as f:
             feed_setting = f.read()
         feedjob = self.engine.create(feed_setting)
         self.assertIsNotNone(feedjob, "feedjob should not be none")
@@ -36,7 +42,8 @@ class SimpleFileFeedTest(unittest.TestCase):
 
     def test_fetchonly(self):
         setting_file = 'fetchonly.feed.json'
-        with open(setting_file, 'r') as f:
+        file_path = self.get_conf_file(setting_file)
+        with open(file_path, 'r') as f:
             feed_setting = f.read()
         feedjob = self.engine.create(feed_setting)
         self.assertIsNotNone(feedjob, "feedjob should not be none")
@@ -46,7 +53,8 @@ class SimpleFileFeedTest(unittest.TestCase):
     
     def test_loop(self):
         setting_file = 'loop.feed.json'
-        with open(setting_file, 'r') as f:
+        file_path = self.get_conf_file(setting_file)
+        with open(file_path, 'r') as f:
             feed_setting = f.read()
         feedjob = self.engine.create(feed_setting)
         self.assertIsNotNone(feedjob, "feedjob should not be none")
